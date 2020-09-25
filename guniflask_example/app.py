ddc5ef85e6a2c2730f45eb5bcbd632f2b6d9d2e9
jadbin <jadbin.com@hotmail.com>
2020-09-25 15:40:23 +0800

    reinit

--- guniflask_example/app.py
+++ guniflask_example/app.py
@@ -3,20 +3,22 @@
 import logging
 from os.path import join, dirname
 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from guniflask.config import Settings
 
 log = logging.getLogger(__name__)
 
 db = SQLAlchemy()
 
 
-def make_settings(app, settings):
+def make_settings(app: Flask, settings: Settings):
     """
     This function is invoked before initializing app.
     """
 
 
-def init_app(app, settings):
+def init_app(app: Flask, settings: Settings):
     """
     This function is invoked before running app.
     """
